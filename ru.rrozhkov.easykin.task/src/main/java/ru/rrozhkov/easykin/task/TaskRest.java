5b8d7f4d0f0a7a53f6250b668501e4eec7298ace
rrozhkov <rrozhkov@primeaspect.io>
2020-06-16 14:13:04 +0600

    EKCL-0003 Add swagger

--- ru.rrozhkov.easykin.task/src/main/java/ru/rrozhkov/easykin/task/TaskRest.java
+++ ru.rrozhkov.easykin.task/src/main/java/ru/rrozhkov/easykin/task/TaskRest.java
@@ -1,9 +1,9 @@
 package ru.rrozhkov.easykin.task;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,12 +15,12 @@ public class TaskRest {
     @Autowired
     private TaskDao taskDao;
 
-    @RequestMapping(value = "/tasks", method = RequestMethod.GET)
+    @PostMapping("/tasks")
     public List<? extends ITask> getTasks(@RequestParam Map<String, String> params) {
         return taskDao.search(params);
     }
 
-    @RequestMapping("/task/{id}")
+    @GetMapping("/task/{id}")
     public ITask getTask(@PathVariable Integer id) {
         return taskDao.findById(id);
     }
